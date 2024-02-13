dd7b177280be12e55e43cc0634acbdd7307a1b80
Myllena Corrêa <132961839+myllenacorrea@users.noreply.github.com>
2024-02-13 16:55:27 -0300

    Create main.go

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var a string // definindo o tipo da varivel
+var d int
+
+func main() {
+	a = "Myllena" // atribuindo um valor a variavel
+	b := 10       // define o tipo e atribui o valor ao mesmo tempo
+	c := 2
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(b + c)
+}
+
+//comentario
